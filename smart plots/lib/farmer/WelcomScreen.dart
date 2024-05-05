ab858611c0313512b407cd06ac3044a297dff3f9
Rafik Hijazy <rafikhz90@gmail.com>
2024-05-06 01:20:18 +0300

    use MyTitle Rather than the old implementation

--- smart plots/lib/farmer/WelcomScreen.dart
+++ smart plots/lib/farmer/WelcomScreen.dart
@@ -10,7 +10,7 @@ class WelcomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar:AppBar(toolbarHeight: 200,title:Container(child:Center(child: Column(children: [Logo(),],)),)),
-      body: Container(child: Center(child: Column(mainAxisAlignment: MainAxisAlignment.spaceBetween,children: [Column(children:<Widget>[,Text("Welcome Back",style: TextStyle(fontSize: 34,fontWeight: FontWeight.bold),),Text("Please Enter Your Personal data",style: TextStyle(fontSize: 18,fontWeight: FontWeight.normal,fontFamily: 'Nunito'))],),Button(onPress:(){
+      body: Container(child: Center(child: Column(mainAxisAlignment: MainAxisAlignment.spaceBetween,children: [Column(children:<Widget>[MyTitle(text: "Welcome Back"),Text("Please Enter Your Personal data",style: TextStyle(fontSize: 18,fontWeight: FontWeight.normal,fontFamily: 'Nunito'))],),Button(onPress:(){
         Navigator.of(context).pushReplacementNamed("/signin");
       }, text:"Get Started"),],),),
     ));
