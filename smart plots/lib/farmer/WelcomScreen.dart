aa3596a936c4f3edf91f5227b07d82c279866162
Rafik Hijazy <rafikhz90@gmail.com>
2024-05-06 01:27:28 +0300

    try MyText Widget

--- smart plots/lib/farmer/WelcomScreen.dart
+++ smart plots/lib/farmer/WelcomScreen.dart
@@ -3,6 +3,7 @@ import '../comon/Button.dart';
 import '../comon/TextInputs.dart';
 import '../comon/Logo.dart';
 import '../comon/MyTitle.dart';
+import '../comon/MyText.dart';
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({super.key});
 
@@ -10,7 +11,7 @@ class WelcomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar:AppBar(toolbarHeight: 200,title:Container(child:Center(child: Column(children: [Logo(),],)),)),
-      body: Container(child: Center(child: Column(mainAxisAlignment: MainAxisAlignment.spaceBetween,children: [Column(children:<Widget>[MyTitle(text: "Welcome Back"),Text("Please Enter Your Personal data",style: TextStyle(fontSize: 18,fontWeight: FontWeight.normal,fontFamily: 'Nunito'))],),Button(onPress:(){
+      body: Container(child: Center(child: Column(mainAxisAlignment: MainAxisAlignment.spaceBetween,children: [Column(children:<Widget>[MyTitle(text: "Welcome Back"),MyText(text: "Please Enter Your Personal data"),Text("Please Enter Your Personal data",style: TextStyle(fontSize: 18,fontWeight: FontWeight.normal,fontFamily: 'Nunito'))],),Button(onPress:(){
         Navigator.of(context).pushReplacementNamed("/signin");
       }, text:"Get Started"),],),),
     ));
