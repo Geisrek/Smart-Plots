fe0abf6b67fa51229baaa44f77083df7fb6469d7
Rafik Hijazy <rafikhz90@gmail.com>
2024-05-05 22:42:16 +0300

    adjust the Button attribute

--- smart plots/lib/WelcomPage.dart
+++ smart plots/lib/WelcomPage.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import './Button.dart';
 class WelcomePage extends StatelessWidget {
   const WelcomePage({super.key});
 
@@ -10,7 +10,7 @@ class WelcomePage extends StatelessWidget {
         child:Image.asset("./images/Smartplots.png"),
         
       )),],)),)),
-      body: Container(child: Center(child: Column(mainAxisAlignment: MainAxisAlignment.spaceBetween,children: [Column(children:<Widget>[Text("Welcome Back",style: TextStyle(fontSize: 34,fontWeight: FontWeight.bold),),Text("Please Enter Your Personal data",style: TextStyle(fontSize: 18,fontWeight: FontWeight.normal,fontFamily: 'Nunito'))],),Text("Buttone")],),),
+      body: Container(child: Center(child: Column(mainAxisAlignment: MainAxisAlignment.spaceBetween,children: [Column(children:<Widget>[Text("Welcome Back",style: TextStyle(fontSize: 34,fontWeight: FontWeight.bold),),Text("Please Enter Your Personal data",style: TextStyle(fontSize: 18,fontWeight: FontWeight.normal,fontFamily: 'Nunito'))],),Button(onPress:(){}, text:"Get Started")],),),
     ));
   }
 }
