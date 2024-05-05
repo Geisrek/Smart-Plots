6a19f77be284b0c9c591a44ffb0c1342eaf4c034
Rafik Hijazy <rafikhz90@gmail.com>
2024-05-05 22:08:07 +0300

    styling  the button widget

--- smart plots/lib/Button.dart
+++ smart plots/lib/Button.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class Button extends StatelessWidget {
-  const Button({super.key});
+  final onPress;
+  final text;
+  const Button({super.key,required this.onPress,required this.text});
 
   @override
   Widget build(BuildContext context) {
-    return button;
+    return Container(height: 48,width: 350,child:  ElevatedButton(onPressed: this.onPress, child: Text(this.text)),decoration: BoxDecoration(),);
   }
 }
