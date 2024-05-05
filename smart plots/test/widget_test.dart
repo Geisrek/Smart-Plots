415519d16425fb679c4573f06347e6f39137ddbf
Rafik Hijazy <rafikhz90@gmail.com>
2024-05-06 01:26:02 +0300

    update MyText Widget return

--- smart plots/test/widget_test.dart
+++ smart plots/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:Smar pluts/main.dart';
+import 'package:smart plots/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
