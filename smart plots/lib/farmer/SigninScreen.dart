d4912a23e577c308bb22b25748ce09cb0e6bc222
Rafik Hijazy <rafikhz90@gmail.com>
2024-05-06 01:02:40 +0300

    start emplemnting the signin scaffold body

--- smart plots/lib/farmer/SigninScreen.dart
+++ smart plots/lib/farmer/SigninScreen.dart
@@ -7,6 +7,7 @@ class SigninScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(toolbarHeight: 200,title: Center(child: Logo(),),),
+      body: Container(child: Column(children: <Widget>[],),),
     );
   }
 }
