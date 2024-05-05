a3506ea7e6ebd2a7b06a3e3c63876752385aaa78
Rafik Hijazy <rafikhz90@gmail.com>
2024-05-06 01:23:34 +0300

    update MyText Widget name

--- smart plots/lib/comon/MyText.dart
+++ smart plots/lib/comon/MyText.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+class MyText extends StatelessWidget {
+  final String text;
+  const MyText({super.key,required this.text});
 
   @override
   Widget build(BuildContext context) {
