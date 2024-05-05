23d1b58da91da9a8e63e591ea3a7dfc057df2fc9
Rafik Hijazy <rafikhz90@gmail.com>
2024-05-06 01:32:12 +0300

    add title to signinScreen

--- smart plots/lib/farmer/SigninScreen.dart
+++ smart plots/lib/farmer/SigninScreen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import '../comon/Logo.dart';
 import '../comon/TextInputs.dart';
+import '../comon/MyTitle.dart';
 class SigninScreen extends StatelessWidget {
   const SigninScreen({super.key});
 
@@ -9,7 +10,7 @@ class SigninScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(toolbarHeight: 200,title: Center(child: Logo(),),),
-      body: Container(child: Center(child: Column(children: <Widget>[SizedBox(height: 20,),InputText(),SizedBox(height: 20,),PasswordInput()],)),),
+      body: Container(child: Center(child: Column(children: <Widget>[MyTitle(text: "Welcome Back"),SizedBox(height: 20,),InputText(),SizedBox(height: 20,),PasswordInput()],)),),
     );
   }
 }
