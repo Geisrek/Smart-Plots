2ac70cc634c8529e7da46f4955950b5a5b4d96a4
Rafik Hijazy <rafikhz90@gmail.com>
2024-05-06 00:44:33 +0300

    replase code in Welcome page to the created logo widget

--- smart plots/lib/farmer/WelcomScreen.dart
+++ smart plots/lib/farmer/WelcomScreen.dart
@@ -1,16 +1,14 @@
 import 'package:flutter/material.dart';
 import '../comon/Button.dart';
 import '../comon/TextInputs.dart';
+import '../comon/Logo.dart';
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar(toolbarHeight: 200,title:Container(child:Center(child: Column(children: [Container(width: 150,height: 200,child:ClipRRect(
-        child:Image.asset("./images/Smartplots.png"),
-        
-      )),],)),)),
+      appBar:AppBar(toolbarHeight: 200,title:Container(child:Center(child: Column(children: [Logo(),],)),)),
       body: Container(child: Center(child: Column(mainAxisAlignment: MainAxisAlignment.spaceBetween,children: [Column(children:<Widget>[Text("Welcome Back",style: TextStyle(fontSize: 34,fontWeight: FontWeight.bold),),Text("Please Enter Your Personal data",style: TextStyle(fontSize: 18,fontWeight: FontWeight.normal,fontFamily: 'Nunito'))],),Button(onPress:(){}, text:"Get Started"),],),),
     ));
   }
