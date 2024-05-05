98d4fd70f2a44d08eb0e3cc96299dba59532df6c
Rafik Hijazy <rafikhz90@gmail.com>
2024-05-06 00:50:19 +0300

    set appBar child to logo

--- smart plots/lib/farmer/SigninScreen.dart
+++ smart plots/lib/farmer/SigninScreen.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
-
+import '../comon/Logo.dart';
 class SigninScreen extends StatelessWidget {
   const SigninScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(toolbarHeight: 200,title: Center(child: ,),),
+      appBar: AppBar(toolbarHeight: 200,title: Center(child: Logo(),),),
     );
   }
 }
