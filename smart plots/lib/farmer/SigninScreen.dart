50f53cd9be0d0fd00d6ce6ef97536711778590a0
Rafik Hijazy <rafikhz90@gmail.com>
2024-05-06 01:05:27 +0300

    fill the text field in  signin scaffold body

--- smart plots/lib/farmer/SigninScreen.dart
+++ smart plots/lib/farmer/SigninScreen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import '../comon/Logo.dart';
+import '../comon/TextInputs.dart';
 class SigninScreen extends StatelessWidget {
   const SigninScreen({super.key});
 
@@ -7,7 +9,7 @@ class SigninScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(toolbarHeight: 200,title: Center(child: Logo(),),),
-      body: Container(child: Column(children: <Widget>[],),),
+      body: Container(child: Column(children: <Widget>[InputText(),PasswordInput()],),),
     );
   }
 }
