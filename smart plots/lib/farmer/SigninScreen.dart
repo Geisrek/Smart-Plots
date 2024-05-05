50975bcde4478e60f13f78607375bd5a7989b6ea
Rafik Hijazy <rafikhz90@gmail.com>
2024-05-06 00:47:53 +0300

    start implement the appBar in SigninScreen

--- smart plots/lib/farmer/SigninScreen.dart
+++ smart plots/lib/farmer/SigninScreen.dart
@@ -6,7 +6,7 @@ class SigninScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(title: Center(child: ,),),
     );
   }
 }
