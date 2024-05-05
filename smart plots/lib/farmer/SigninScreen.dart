f8348e8372330c3ad363023d23956ee79681dc4a
Rafik Hijazy <rafikhz90@gmail.com>
2024-05-06 02:13:51 +0300

    add login button to signin widget

--- smart plots/lib/farmer/SigninScreen.dart
+++ smart plots/lib/farmer/SigninScreen.dart
@@ -4,14 +4,15 @@ import '../comon/Logo.dart';
 import '../comon/TextInputs.dart';
 import '../comon/MyTitle.dart';
 import '../comon/MyText.dart';
+import '../comon/Button.dart';
 class SigninScreen extends StatelessWidget {
   const SigninScreen({super.key});
-
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(toolbarHeight: 200,title: Center(child: Logo(),),),
-      body: Container(child: Center(child: Column(children: <Widget>[MyTitle(text: "Welcome back !"),MyText(text: "Glad to See you !"),SizedBox(height: 20,),InputText(),SizedBox(height: 20,),PasswordInput()],)),),
+      body: Container(child: Center(child: Column(children: <Widget>[MyTitle(text: "Welcome back !"),MyText(text: "Glad to See you !"),SizedBox(height: 20,),InputText(),SizedBox(height: 20,),PasswordInput(),SizedBox(height: 20,),Button(onPress: (){}, text: "Login")],)),),
     );
   }
 }
