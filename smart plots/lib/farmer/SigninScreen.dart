e626d0e89e3a2cebac0093e5cd70ecc8cd440e68
Rafik Hijazy <rafikhz90@gmail.com>
2024-05-06 04:10:15 +0300

    create te sotial registration buttons

--- smart plots/lib/farmer/SigninScreen.dart
+++ smart plots/lib/farmer/SigninScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import '../comon/Logo.dart';
 import '../comon/TextInputs.dart';
 import '../comon/MyTitle.dart';
@@ -13,7 +14,7 @@ class SigninScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(toolbarHeight: 200,title: Center(child: Logo(),),),
-      body: Container(child: Center(child: Column(children: <Widget>[MyTitle(text: "Welcome back !"),MyText(text: "Glad to See you !"),SizedBox(height: 20,),email,SizedBox(height: 20,),password,SizedBox(height: 20,),Button(onPress: (){print(password.getText());}, text: "Login")],)),),
+      body: Container(child: Center(child: Column(children: <Widget>[MyTitle(text: "Welcome back !"),MyText(text: "Glad to See you !"),SizedBox(height: 20,),email,SizedBox(height: 20,),password,SizedBox(height: 20,),Column(children:[Button(onPress: (){}, text: "Login"),Row(children: [MyText(text: "")],)]),Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,children: [Container(width: 130,child: Divider(thickness: 2,),),MyText(text: "Signin with"),Container(width: 130,child: Divider(thickness: 2,),)]),Container(margin: EdgeInsets.only(left: 80,top: 10),child: Row(children: [Container(width: 85,height: 40,decoration: BoxDecoration(borderRadius: BorderRadius.circular(7),color: Color(0xFF000000)),child: ElevatedButton(style: ElevatedButton.styleFrom(elevation: 0,backgroundColor: Color(0x0000)),onPressed: (){},child: SvgPicture.asset('./images/google.svg',),),),SizedBox(width: 20,),Container(width: 85,height: 40,decoration: BoxDecoration(borderRadius: BorderRadius.circular(7),color: Color(0xFF000000)),child: ElevatedButton(style: ElevatedButton.styleFrom(elevation: 0,backgroundColor: Color(0x0000)),onPressed: (){},child: SvgPicture.asset('./images/facebook.svg',),))],)),])),)
     );
   }
 }
