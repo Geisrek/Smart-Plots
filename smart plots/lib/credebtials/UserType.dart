88e4579fed8290702d75cbdc5e52c337dab093b7
Rafik Hijazy <rafikhz90@gmail.com>
2024-05-06 23:45:19 +0300

    create effect for user type

--- smart plots/lib/credebtials/UserType.dart
+++ smart plots/lib/credebtials/UserType.dart
@@ -9,16 +9,27 @@ class UserType extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return InkWell( 
-      onTap: (){print("bruh");
-      
+      onTap: (){
+        
+        this.color.withOpacity(1);
       }, 
       child:  Container(height: 170,
     width: 350,
+    padding: EdgeInsets.only(left: 5),
+    
     decoration: BoxDecoration(
+      
       borderRadius: BorderRadius.circular(7),color:this.color ),
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.center,
-        children: [Container(height: 60,width: 60,child: ClipRRect(child: Image.asset(this.path),),),Text(this.text,style: TextStyle(color: Colors.white,fontFamily: 'Nunito',fontSize: 18,fontWeight: FontWeight.bold),)],),
+        children: [Container(height: 50,
+        width: 50,
+         child: ClipRRect(
+          child: Image.asset(this.path),borderRadius:BorderRadius.circular(100) ,),),SizedBox(width: 200,),
+         Text(this.text,style: TextStyle(color: Colors.white,
+         fontFamily: 'Nunito',
+         fontSize: 18,
+         fontWeight: FontWeight.bold),)],),
         ),
       );
   }
