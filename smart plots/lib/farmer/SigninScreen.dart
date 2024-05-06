448bdcae69568a94e6bb80aa15b53bdb0ddb941f
Rafik Hijazy <rafikhz90@gmail.com>
2024-05-06 16:56:15 +0300

    modify the signup with button in sign in screen

--- smart plots/lib/farmer/SigninScreen.dart
+++ smart plots/lib/farmer/SigninScreen.dart
@@ -26,11 +26,11 @@ class SigninScreen extends StatelessWidget {
           SizedBox(height: 20,),
           Button(onPress: (){}, text: "Login"),
           Container(padding: 
-          EdgeInsets.only(left: 10,right: 10), margin: EdgeInsets.only(bottom: 25,left: 5),height: 40,child:
+          EdgeInsets.only(left: 10,right: 10), margin: EdgeInsets.only(bottom: 10,right: 10),height: 40,child:
             Row(crossAxisAlignment: CrossAxisAlignment.center,children: 
             [Text( "do you want to create an account?",style: TextStyle(fontSize: 16,fontFamily: 'Nunito') ,),
-            TextButton(style: TextButton.styleFrom(fixedSize: Size(85, 80)),child: 
-            Text("Signup",style:TextStyle(fontSize: 18,color: Colors.black,fontFamily: 'Nunito')),onPressed: (){Navigator.of(context).pushReplacementNamed("/signup");},)],)),
+            TextButton(style: TextButton.styleFrom(fixedSize: Size(75, 80)),child: 
+            Text("Signup",style:TextStyle(fontSize: 16,color: Colors.black,fontFamily: 'Nunito')),onPressed: (){Navigator.of(context).pushReplacementNamed("/signup");},)],)),
             Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,children:
              [Container(width: 130,child: Divider(thickness: 2,),)
              ,MyText(text: "Signin with"),
