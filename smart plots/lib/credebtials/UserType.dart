273d157904ccb524177dd687e376069762ca7d63
Rafik Hijazy <rafikhz90@gmail.com>
2024-05-07 00:29:35 +0300

    finish the PickUserType widget

--- smart plots/lib/credebtials/UserType.dart
+++ smart plots/lib/credebtials/UserType.dart
@@ -1,36 +1,56 @@
 import 'package:flutter/material.dart';
 
-class UserType extends StatelessWidget {
+class UserType extends StatefulWidget {
   final  Color color;
   final String  text;
   final String  path;
-  const UserType({super.key,required this.color,required this.text,required this.path});
- 
+  UserType({super.key,required this.color,required this.text,required this.path});
+  State<UserType> createState() => _UserType();
+  }
+  class _UserType extends State<UserType> {
+   double width=0;
   @override
   Widget build(BuildContext context) {
     return InkWell( 
       onTap: (){
-        
-        this.color.withOpacity(1);
+        setState(() {
+           this.width=this.width>0?0:3;
+        });
+       
+        print(this.width);
       }, 
       child:  Container(height: 170,
     width: 350,
     padding: EdgeInsets.only(left: 5),
     
     decoration: BoxDecoration(
-      
-      borderRadius: BorderRadius.circular(7),color:this.color ),
+      border: Border.all(width: this.width,color: Color.fromARGB(225, 100, 200, 150)),
+      borderRadius: BorderRadius.circular(7),color:widget.color ),
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [Container(height: 50,
         width: 50,
          child: ClipRRect(
-          child: Image.asset(this.path),borderRadius:BorderRadius.circular(100) ,),),SizedBox(width: 200,),
-         Text(this.text,style: TextStyle(color: Colors.white,
+          child: Image.asset(widget.path),borderRadius:BorderRadius.circular(100) ,),),SizedBox(width: 200,),
+         Text(this.widget.text,style: TextStyle(color: Colors.white,
          fontFamily: 'Nunito',
          fontSize: 18,
          fontWeight: FontWeight.bold),)],),
         ),
       );
+  }
+}
+
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
