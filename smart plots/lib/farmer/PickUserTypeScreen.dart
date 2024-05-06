7224e65e4b992bce2976958dfd36e88fa20a1c17
Rafik Hijazy <rafikhz90@gmail.com>
2024-05-06 17:23:56 +0300

    create title for app bar in scaffold in  pick user type screen structure

--- smart plots/lib/farmer/PickUserTypeScreen.dart
+++ smart plots/lib/farmer/PickUserTypeScreen.dart
@@ -5,6 +5,6 @@ class MyWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold( appBar: AppBar(),);
+    return Scaffold( appBar: AppBar(title: Container(),),);
   }
 }
