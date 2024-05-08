26ad0f94acc646e591d001181a3054187f42f000
Rafik Hijazy <rafikhz90@gmail.com>
2024-05-08 20:43:33 +0300

    call padding for informations container

--- smart plots with flutter/lib/vendor/VendorsScreen.dart
+++ smart plots with flutter/lib/vendor/VendorsScreen.dart
@@ -28,6 +28,7 @@ class _VendorScreenState extends State<VendorScreen> {
           SizedBox(height: 10,),
           Container(width: 300,
           height: 60 ,
+          padding: EdgeInsets,
           decoration: BoxDecoration(color: Color(0xFFD9D9D9),borderRadius:BorderRadius.circular(7)),
           child:Row(
             crossAxisAlignment: CrossAxisAlignment.center ,
