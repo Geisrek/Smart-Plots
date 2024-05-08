93bf4baf33077997575b8df625a9286efd775cea
Rafik Hijazy <rafikhz90@gmail.com>
2024-05-09 02:58:46 +0300

    create Column widget in the ClientScreen title

--- smart plots with flutter/lib/client/ClientScreen.dart
+++ smart plots with flutter/lib/client/ClientScreen.dart
@@ -8,7 +8,7 @@ class ClientScreen extends StatelessWidget {
     return  Scaffold(
       appBar: AppBar(
         title: Center(
-          child: ,
+          child: Column(),
         ),),
     );
   }
