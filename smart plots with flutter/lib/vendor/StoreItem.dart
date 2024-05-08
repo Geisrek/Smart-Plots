db94b51f4bf245c89c851cf65500d5da2ea90329
Rafik Hijazy <rafikhz90@gmail.com>
2024-05-09 01:52:00 +0300

    call padding to second item StoreItem

--- smart plots with flutter/lib/vendor/StoreItem.dart
+++ smart plots with flutter/lib/vendor/StoreItem.dart
@@ -20,6 +20,7 @@ class StoreItem extends StatelessWidget {
           MyText(text: this.price)
           ],
         ),Container(height: 70,width:200 ,
+        padding: ,
         decoration: BoxDecoration(color: Colors.white,borderRadius: BorderRadius.circular(7))
         ,child: Row(mainAxisAlignment:MainAxisAlignment.spaceBetween 
         ,crossAxisAlignment: CrossAxisAlignment.center
