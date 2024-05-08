459929adf0e19711bf0545588c1cda069796e702
Rafik Hijazy <rafikhz90@gmail.com>
2024-05-09 01:12:37 +0300

    add child attribut to the Expanded

--- smart plots with flutter/lib/vendor/VendorStoreScreen.dart
+++ smart plots with flutter/lib/vendor/VendorStoreScreen.dart
@@ -16,7 +16,9 @@ class _VendorStoreScreenState extends State<VendorStoreScreen> {
        body:Container(
         width:400 ,
         height: 600,
-        child: Expanded(),
+        child: Expanded(
+          child: ,
+        ),
        ) ,
     );
   }
