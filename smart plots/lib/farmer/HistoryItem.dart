d83499c6d2f9248736bc5ff6c8b047665c0981e1
Rafik Hijazy <rafikhz90@gmail.com>
2024-05-08 09:37:40 +0300

    Create HistoryItem widget

---
+++ smart plots/lib/farmer/HistoryItem.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HistoryItem extends StatelessWidget {
+  const HistoryItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
