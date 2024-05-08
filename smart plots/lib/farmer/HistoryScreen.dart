a5238f99bcf1b19ac58cd57b4bd823c8f259332c
Rafik Hijazy <rafikhz90@gmail.com>
2024-05-08 10:56:24 +0300

    modify  HistoryScreen  alignment

--- smart plots/lib/farmer/HistoryScreen.dart
+++ smart plots/lib/farmer/HistoryScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../comon/MyTitle.dart';
+import './HistoryItem.dart';
 class HistoryScreen extends StatelessWidget {
   const HistoryScreen({super.key});
 
@@ -12,10 +13,12 @@ class HistoryScreen extends StatelessWidget {
         ),
       ),
       body: Container(height: 600,width: 400,padding: EdgeInsets.all(5),
-      child: Column(crossAxisAlignment: CrossAxisAlignment.center
+      child: Column(crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.center
       ,children: [
         MyTitle(text: "Plot 1"),
-        
+        HistoryItem(Date: "11/3/2015", EC: "10", PH: "12", Lux: "100", C: "24"),
+         HistoryItem(Date: "11/4/2015", EC: "20", PH: "5", Lux: "100", C: "24")
       ],),
       ),
     );
