fbb9626be70deec7b43129112353c551e6b5ca5c
Rafik Hijazy <rafikhz90@gmail.com>
2024-05-08 23:07:24 +0300

    change the widget name

---
+++ smart plots with flutter/lib/vendor/VendorStoreScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class VendorStoreScreen extends StatefulWidget {
+  const VendorStoreScreen({super.key});
+
+  @override
+  State<VendorStoreScreen> createState() => _VendorStoreScreenState();
+}
+
+class _VendorStoreScreenState extends State<VendorStoreScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
