85abf167fbb264904582557ed3017a9bcde3f94f
Rafik Hijazy <rafikhz90@gmail.com>
2024-05-08 11:48:53 +0300

    design the remote body

--- smart plots/lib/farmer/RemoteScreen.dart
+++ smart plots/lib/farmer/RemoteScreen.dart
@@ -7,7 +7,7 @@ class RemoteScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: MyBar(Title: "Remote").MyAppBarr(),
-      body:Container() ,
+      body:Container(width: 400,height: 600,child: Column(children: [],),) ,
     );
   }
 }
