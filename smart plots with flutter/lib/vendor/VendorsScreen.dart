5caf2228fa253b23e007f42a0c918d8cce15e503
Rafik Hijazy <rafikhz90@gmail.com>
2024-05-09 01:28:26 +0300

    create navigatore in the VendorScreen for storage button to VendorsStoreScreen

--- smart plots with flutter/lib/vendor/VendorsScreen.dart
+++ smart plots with flutter/lib/vendor/VendorsScreen.dart
@@ -23,7 +23,7 @@ class _VendorScreenState extends State<VendorScreen> {
           children: [
             MyTitle(text: "Vendor"),
             SizedBox(width: 135,),
-            IconButton(onPressed: (){}, icon: SvgPicture.asset("./images/saved.svg"))
+            IconButton(onPressed: (){Navigator}, icon: SvgPicture.asset("./images/saved.svg"))
           ],
         ),
       ),
