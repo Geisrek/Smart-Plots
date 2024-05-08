ab512c5e0a38eb60353285e307003db5de8a075a
Rafik Hijazy <rafikhz90@gmail.com>
2024-05-08 11:53:47 +0300

    ad the control bar to the the remote body

--- smart plots/lib/farmer/RemoteScreen.dart
+++ smart plots/lib/farmer/RemoteScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import "AppBar.dart";
+import './Controle.dart';
 class RemoteScreen extends StatelessWidget {
   const RemoteScreen({super.key});
 
@@ -7,7 +8,17 @@ class RemoteScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: MyBar(Title: "Remote").MyAppBarr(),
-      body:Container(width: 400,height: 600,child: Column(children: [],),) ,
+      body:Container(width: 400,
+      height: 600,
+      child: Column(
+        children: [
+          Container(
+            height: 460
+            ,width: 390,
+            child: Wrap(children: [Row()],
+            ),
+            )
+            ,Controle()],),) ,
     );
   }
 }
