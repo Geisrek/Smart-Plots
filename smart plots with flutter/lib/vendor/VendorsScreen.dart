defaeab587652a634c19f8333e797c8f99fcc6ff
Rafik Hijazy <rafikhz90@gmail.com>
2024-05-08 20:41:44 +0300

    call crossAxisAlignment for the informations Row

--- smart plots with flutter/lib/vendor/VendorsScreen.dart
+++ smart plots with flutter/lib/vendor/VendorsScreen.dart
@@ -30,6 +30,7 @@ class _VendorScreenState extends State<VendorScreen> {
           height: 60 ,
           decoration: BoxDecoration(color: Color(0xFFD9D9D9),borderRadius:BorderRadius.circular(7)),
           child:Row(
+            crossAxisAlignment: ,
             children: [
               MyText(text: "Strawbary "),
             ],
