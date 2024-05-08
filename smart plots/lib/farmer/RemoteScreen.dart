06aa9400e8ba166e44d30ea584f2821cb52c45d2
Rafik Hijazy <rafikhz90@gmail.com>
2024-05-08 11:39:13 +0300

    Create RemoteScreen

---
+++ smart plots/lib/farmer/RemoteScreen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class RemoteScreen extends StatelessWidget {
+  const RemoteScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
