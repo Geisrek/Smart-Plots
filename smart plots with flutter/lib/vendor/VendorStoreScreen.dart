4e2f082bf1c05e0b179f2660995e010f4fdb90bf
Rafik Hijazy <rafikhz90@gmail.com>
2024-05-09 01:04:13 +0300

    add Container to the body of VendorStoreScreen

--- smart plots with flutter/lib/vendor/VendorStoreScreen.dart
+++ smart plots with flutter/lib/vendor/VendorStoreScreen.dart
@@ -13,7 +13,7 @@ class _VendorStoreScreenState extends State<VendorStoreScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
        appBar: AppBar(title: Center(child: MyTitle(text: "Sored product",),),),
-       body: ,
+       body:Container() ,
     );
   }
 }
