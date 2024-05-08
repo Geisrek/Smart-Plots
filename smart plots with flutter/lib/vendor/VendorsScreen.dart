b5e7cc8bdbbee2f8de8dea661de37d72af727c44
Rafik Hijazy <rafikhz90@gmail.com>
2024-05-08 21:42:20 +0300

    call expanded component  for AI suggestions Container in Vendor Screen

--- smart plots with flutter/lib/vendor/VendorsScreen.dart
+++ smart plots with flutter/lib/vendor/VendorsScreen.dart
@@ -47,6 +47,7 @@ class _VendorScreenState extends State<VendorScreen> {
           margin: EdgeInsets.only(top: 10,bottom: 10),
           width: 300,
           height: 100,
+          child: Expanded(),
         )],) ,
       ) ,
     );
