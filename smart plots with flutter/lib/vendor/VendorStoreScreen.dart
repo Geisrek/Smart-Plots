449702f6551c546ae6a9eb9178fd496d55a81fb2
Rafik Hijazy <rafikhz90@gmail.com>
2024-05-08 23:18:59 +0300

    make VendoreStoreScreen Scaffold

--- smart plots with flutter/lib/vendor/VendorStoreScreen.dart
+++ smart plots with flutter/lib/vendor/VendorStoreScreen.dart
@@ -10,6 +10,6 @@ class VendorStoreScreen extends StatefulWidget {
 class _VendorStoreScreenState extends State<VendorStoreScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold();
   }
 }
