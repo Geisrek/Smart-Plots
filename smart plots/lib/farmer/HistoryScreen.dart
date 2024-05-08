02d29d12170583bc5fd1147a9ef60597f094aed2
Rafik Hijazy <rafikhz90@gmail.com>
2024-05-08 09:28:44 +0300

    Set the History title

---
+++ smart plots/lib/farmer/HistoryScreen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import '../comon/MyTitle.dart';
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Container(
+          child: MyTitle(text: "History"),
+        ),
+      ),
+    );
+  }
+}
