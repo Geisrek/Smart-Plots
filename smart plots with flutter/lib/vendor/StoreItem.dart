70c9bde411431885583e2f90f5606e3f99f6119f
Rafik Hijazy <rafikhz90@gmail.com>
2024-05-08 23:49:38 +0300

    change the widge name

--- smart plots with flutter/lib/vendor/StoreItem.dart
+++ smart plots with flutter/lib/vendor/StoreItem.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+class StoreItem extends StatelessWidget {
+  const StoreItem({super.key});
 
   @override
   Widget build(BuildContext context) {
