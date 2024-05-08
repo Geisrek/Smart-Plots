e065a90c7246c0d6f6bef6081dc53f371c11e25b
Rafik Hijazy <rafikhz90@gmail.com>
2024-05-08 19:48:55 +0300

    justify the vendorscreen padding method all->only

--- smart plots with flutter/lib/vendor/VendorsScreen.dart
+++ smart plots with flutter/lib/vendor/VendorsScreen.dart
@@ -18,7 +18,7 @@ class _VendorScreenState extends State<VendorScreen> {
         title: MyTitle(text: "Vendor"),
       ),
       body:Container(
-        padding: EdgeInsets.all(20)
+        padding: EdgeInsets.only(20)
         ,
         child:Column(children: [
           widget.serial,
