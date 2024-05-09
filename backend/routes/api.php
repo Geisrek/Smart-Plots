89fff6308e259c9b31e20d440c2011497d8a04b8
Rafik Hijazy <rafikhz90@gmail.com>
2024-05-09 21:33:50 +0300

    past routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -13,6 +13,13 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::controller(AuthController::class)->group(function () {
+    Route::post('login', 'login');
+    Route::post('register', 'register');
+    Route::post('logout', 'logout');
+    Route::post('refresh', 'refresh');
+
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
