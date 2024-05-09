226018926d6aaf9c2a05c0ab058c45d4ba5d9016
Rafik Hijazy <rafikhz90@gmail.com>
2024-05-09 04:40:40 +0300

    import MyText to clientScreen

--- smart plots with flutter/lib/client/ClientScreen.dart
+++ smart plots with flutter/lib/client/ClientScreen.dart
@@ -1,7 +1,7 @@
 import 'package:Smart_pluts/comon/MyText.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-
+import '../comon/MyText.dart';
 class ClientScreen extends StatelessWidget {
   const ClientScreen({super.key});
 
