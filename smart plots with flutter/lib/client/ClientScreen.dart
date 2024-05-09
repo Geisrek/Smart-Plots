25248d89fb3f3472f7046200602cd16f7e0a35f1
Rafik Hijazy <rafikhz90@gmail.com>
2024-05-09 04:25:04 +0300

    add container for the vegetable icon

--- smart plots with flutter/lib/client/ClientScreen.dart
+++ smart plots with flutter/lib/client/ClientScreen.dart
@@ -41,7 +41,7 @@ class ClientScreen extends StatelessWidget {
                           MyText(text: "3\$",)
                         ],
                       )
-                    ],
+                    ,Container()],
                   ),
                 )
               ],
