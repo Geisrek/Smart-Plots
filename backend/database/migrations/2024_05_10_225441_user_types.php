e5037d7885142d58efabca05d20ef14f9c49a946
Rafik Hijazy <rafikhz90@gmail.com>
2024-05-11 02:22:40 +0300

    refer the user_id as foreign key for id in users

--- backend/database/migrations/2024_05_10_225441_user_types.php
+++ backend/database/migrations/2024_05_10_225441_user_types.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('user_types',function(Blueprint $table){
           $table->id();
           $table->unsignedunsignedBigInteger("user_id");
-          $table->foreign("user_id")
+          $table->foreign("user_id")->references("id")->on("user");
         });
     }
 
