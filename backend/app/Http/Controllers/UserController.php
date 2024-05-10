7c749bd6190dcc3bda3346dd17793c7403fc1fb5
Rafik Hijazy <rafikhz90@gmail.com>
2024-05-11 01:07:11 +0300

    add user_image to paramiters

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -55,7 +55,7 @@ class UserController extends Controller
             'email' => $request->email,
             'password' => Hash::make($request->password),
             'user_address'=>$request->user_address,
-            
+            'user_image'=>$request->user_image
         ]);
 
         $token = Auth::login($user);
