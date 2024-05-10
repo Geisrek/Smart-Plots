a533b4d9423658e11d6e2f0d7c8c99ad7d2f5049
Rafik Hijazy <rafikhz90@gmail.com>
2024-05-11 02:17:57 +0300

    craete user_types id column

--- backend/database/migrations/2024_05_10_225441_user_types.php
+++ backend/database/migrations/2024_05_10_225441_user_types.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('user_types',function(Blueprint $table){
-
+          $table->id();
         });
     }
 
