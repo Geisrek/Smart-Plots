e412b14cfc0602bc945f62ead328d95c67e7c3d6
Rafik Hijazy <rafikhz90@gmail.com>
2024-05-12 00:32:08 +0300

    create user_id column

--- backend/database/migrations/2024_05_11_212321_create_smart_plots.php
+++ backend/database/migrations/2024_05_11_212321_create_smart_plots.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('smart_plots', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger("user_id");
+            $table->foreign("user_id")->references("user_id")->on("user_types");
+           // $table->string("product");
+           // $table->string("address");
             $table->timestamps();
         });
     }
