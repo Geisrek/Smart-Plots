2eab720d84a8abdb8d93b6398ebd169fae1d3ae7
Rafik Hijazy <rafikhz90@gmail.com>
2024-05-12 00:46:26 +0300

    identify the targeted table in user_plots model

--- backend/app/Http/Controllers/SmartPlotsController.php
+++ backend/app/Http/Controllers/SmartPlotsController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\user_plots;
 class SmartPlotsController extends Controller
 {
     //
--- backend/app/Models/user_plots.php
+++ backend/app/Models/user_plots.php
@@ -8,5 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class user_plots extends Model
 {
     use HasFactory;
+    protected $table = 'smart_plots';
     protected $fillable=['user_id','product','address'];
 }
