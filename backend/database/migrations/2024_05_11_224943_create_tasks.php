88c7a49f100ee965ea93f3026af4d2ff423ae53c
Rafik Hijazy <rafikhz90@gmail.com>
2024-05-12 01:53:11 +0300

    add sol_humidity column to  tasks migration

--- backend/database/migrations/2024_05_11_224943_create_tasks.php
+++ backend/database/migrations/2024_05_11_224943_create_tasks.php
@@ -14,7 +14,8 @@ return new class extends Migration
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger("light");
-            
+            $table->unsignedBigInteger("sol_humidity");
+
             $table->timestamps();
         });
     }
