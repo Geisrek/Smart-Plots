14b68359e2aab8d0777afff67ba5dd768bc489b3
Rafik Hijazy <rafikhz90@gmail.com>
2024-05-12 02:43:32 +0300

    fix create

--- backend/app/Http/Controllers/TasksController.php
+++ backend/app/Http/Controllers/TasksController.php
@@ -8,7 +8,7 @@ use App\Models\tasks;
 class TasksController extends Controller
 {
     function createTask(Request $req){
-        tasks::craete([
+        tasks::create([
             'light'=>$req->light,
             'sol_humidity'=>$req->sol_humidity,
             'air_humidity'=>$req->air_humidity,
