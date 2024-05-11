089cb8f6d5f80d94c385c856dcd5849031beac59
Rafik Hijazy <rafikhz90@gmail.com>
2024-05-12 00:36:57 +0300

    add address to smart_plots fillable array

--- backend/app/Models/user_plots.php
+++ backend/app/Models/user_plots.php
@@ -8,5 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class user_plots extends Model
 {
     use HasFactory;
-    protected $fillable=['user_id','product'];
+    protected $fillable=['user_id','product','address'];
 }
