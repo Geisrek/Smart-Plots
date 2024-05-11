a561e76ebae8ec522db2577fa88f6bf4b850f8de
Rafik Hijazy <rafikhz90@gmail.com>
2024-05-12 00:53:38 +0300

    implement CreatePlot function

--- backend/app/Http/Controllers/SmartPlotsController.php
+++ backend/app/Http/Controllers/SmartPlotsController.php
@@ -7,6 +7,13 @@ use App\Models\user_plots;
 class SmartPlotsController extends Controller
 {
     function CreatePlot(Request $req){
-
+       $user_id=$req->user_id;
+       $address=$req->address;
+       $product=$req->product;
+       user_plots::create([
+        'user_id'=>$user_id,
+        'address'=>$address,
+        'product'=>$product
+       ]);
     }
 }
