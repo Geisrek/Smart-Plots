601a1ebe4139791b447b0d9537ddbd1c4199a90b
Rafik Hijazy <rafikhz90@gmail.com>
2024-09-15 04:11:31 +0300

    Finish the esp32 remoter buttone api

--- ESP32/Remote/Remote.ino
+++ ESP32/Remote/Remote.ino
@@ -1,17 +1,29 @@
 #include <WiFi.h>
 #include <ESPAsyncWebServer.h>
 
-#define LED_PIN 2
+#define FAN_PIN 3
+#define WATER_PIN 6
+#define CONDITION_PIN 7
+#define TENT_PIN 10
+#define LIGHT_PIN 11
 const char *ssid = "Zaatari net 71503123(hijazi)"; // Change this
 const char *password = "20003000"; 
 AsyncWebServer server(80);
-int LED_state = LOW;
+
 
 
 
 void setup() {
-   pinMode(LED_PIN, OUTPUT);
-    digitalWrite(LED_PIN, LED_state);
+   pinMode(FAN_PIN, OUTPUT);
+   pinMode(WATER_PIN, OUTPUT);
+   pinMode(CONDITION_PIN, OUTPUT);
+   pinMode(TENT_PIN, OUTPUT);
+   pinMode(LIGHT_PIN, OUTPUT);
+   digitalWrite(FAN_PIN,LOW);
+   digitalWrite(WATER_PIN,LOW);
+   digitalWrite(CONDITION_PIN,LOW);
+   digitalWrite(TENT_PIN,LOW);
+  digitalWrite(LIGHT_PIN, LOW);
     Serial.begin(9600);
     WiFi.begin(ssid, password);
     while (WiFi.status() != WL_CONNECTED) {
@@ -21,19 +33,25 @@ void setup() {
     Serial.print("ESP32 Web Server's IP address: ");
     Serial.println(WiFi.localIP());
 
-    server.on("/", HTTP_GET, [](AsyncWebServerRequest * request) {
-        request->send(200, "text/html", getHTML());
-    });
-
     
     server.on("/fan", HTTP_GET, [](AsyncWebServerRequest * request) {
-      if(LED_state==HIGH){
-      digitalWrite(LED_PIN, LOW);
-        LED_state = LOW;}
-      else{
-        digitalWrite(LED_PIN, HIGH);
-        LED_state = HIGH;
-      }
+     pinHandler(FAN_PIN);
+        
+    });
+    server.on("/water", HTTP_GET, [](AsyncWebServerRequest * request) {
+     pinHandler(WATER_PIN);
+        
+    });
+    server.on("/condition", HTTP_GET, [](AsyncWebServerRequest * request) {
+     pinHandler(CONDITION_PIN);
+        
+    });
+    server.on("/tent", HTTP_GET, [](AsyncWebServerRequest * request) {
+     pinHandler(TENT_PIN);
+        
+    });
+    server.on("/light", HTTP_GET, [](AsyncWebServerRequest * request) {
+     pinHandler(LIGHT_PIN);
         
     });
 
@@ -44,4 +62,14 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
+}
+void pinHandler(int PIN){
+
+  if(digitalRead(PIN)==HIGH){
+      digitalWrite(PIN, LOW);
+      }
+      else{
+        digitalWrite(PIN, HIGH);
+      
+      }
 }
