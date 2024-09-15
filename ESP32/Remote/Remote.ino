9e30ae61bd3009c5d6b6337b995ff72614835285
Rafik Hijazy <rafikhz90@gmail.com>
2024-09-15 05:21:12 +0300

    Implement temperature & humidity censors apis HDT11

--- ESP32/Remote/Remote.ino
+++ ESP32/Remote/Remote.ino
@@ -1,16 +1,53 @@
 #include <WiFi.h>
 #include <ESPAsyncWebServer.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+
+
+
 
 #define FAN_PIN 3
 #define WATER_PIN 6
 #define CONDITION_PIN 7
 #define TENT_PIN 10
 #define LIGHT_PIN 11
+#define DHTPIN 26
+#define DHTTYPE    DHT11   
+DHT dht(DHTPIN, DHTTYPE);
+
 const char *ssid = "Zaatari net 71503123(hijazi)"; // Change this
 const char *password = "20003000"; 
 AsyncWebServer server(80);
 
 
+
+
+String readDHTTemperature() {
+  
+  float t = dht.readTemperature();
+  
+  if (isnan(t)) {    
+    Serial.println("Failed to read from DHT sensor!");
+    return "--";
+  }
+  else {
+    Serial.println(t);
+    return String(t);
+  }
+}
+
+String readDHTHumidity() {
+
+  float h = dht.readHumidity();
+  if (isnan(h)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return "--";
+  }
+  else {
+    Serial.println(h);
+    return String(h);
+  }
+}
 
 
 void setup() {
@@ -24,7 +61,9 @@ void setup() {
    digitalWrite(CONDITION_PIN,LOW);
    digitalWrite(TENT_PIN,LOW);
   digitalWrite(LIGHT_PIN, LOW);
-    Serial.begin(9600);
+    Serial.begin(115200);
+
+  dht.begin();
     WiFi.begin(ssid, password);
     while (WiFi.status() != WL_CONNECTED) {
         delay(1000);
@@ -32,6 +71,13 @@ void setup() {
     }
     Serial.print("ESP32 Web Server's IP address: ");
     Serial.println(WiFi.localIP());
+  
+  server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", readDHTTemperature().c_str());
+  });
+  server.on("/humidity", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", readDHTHumidity().c_str());
+  });
 
     
     server.on("/fan", HTTP_GET, [](AsyncWebServerRequest * request) {
